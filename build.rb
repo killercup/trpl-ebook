4d3d0a9d57e9d3196cac88dbe0c465f3406bf60b
Pascal Hertleif <killercup@gmail.com>
2015-05-15 16:59:02 +0200

    Make Index Page More Fancy

--- build.rb
+++ build.rb
@@ -1,7 +1,7 @@
 #! env ruby
 
 # The date of the src/ files
-RELEASE_DATE = "2015-05-13"
+RELEASE_DATE = "2015-05-15"
 
 TOC_LINK_REGEX = /(?<indent>\s*?)\* \[(?<title>.+?)\]\((?<filename>.+?)\)/
 
@@ -99,6 +99,25 @@ puts "[✓] PDF (A4)"
 `pandoc dist/trpl-#{RELEASE_DATE}.md --from=#{MARKDOWN_OPTIONS} --smart --normalize --standalone --self-contained --highlight-style=tango --chapters --table-of-contents --variable monofont='DejaVu Sans Mono' --variable papersize='letterpaper' --template=lib/template.tex --latex-engine=xelatex --to=latex --output=dist/trpl-#{RELEASE_DATE}-letter.pdf`
 puts "[✓] PDF (Letter)"
 
+FILE_PREFIX = /^trpl-(?<date>(\d+)-(\d+)-(\d+))/
+FILE_NAME = /^trpl-(?<date>(\d+)-(\d+)-(\d+))(?<name>.*)/
+
+file_listing = Dir["dist/trpl*"]
+    .map{|f| f.gsub("dist/", "") }
+    .sort.reverse
+    .group_by {|f| f[FILE_PREFIX] }
+    .reject
+    .map {|prefix, files|
+        html = "<li><h2>#{prefix.match(FILE_PREFIX)[:date]}</h2><ul>"
+        html << files.map {|file|
+            "<li><a href='#{file}'>#{
+                file.match(FILE_NAME)[:name].gsub('-', '').gsub('.', ' ').upcase
+            }</a></li>"
+        }.join("\n")
+        html << "</ul></li>"
+        html
+    }.join("\n")
+
 index = <<-eos
 <!DOCTYPE html>
 <html lang="en">
@@ -116,10 +135,7 @@ index = <<-eos
         <li>
             <strong><a href="http://doc.rust-lang.org/book/">The original on rust-lang.org</a></strong>
         </li>
-        #{Dir["dist/trpl*"].map {|file|
-            file = file.gsub("dist/", "")
-            "<li><a href='#{file}'>#{file}</li>"
-        }.join("\n")}
+        #{file_listing}
     </ul>
     <a href="https://github.com/killercup/trpl-ebook">
         <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"/>
