292db37f3b6233b27586133ef533b4a39fa06f52
Pascal Hertleif <killercup@gmail.com>
2015-05-17 17:56:39 +0200

    Render To Single Markdown File

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ extern crate regex;
 extern crate itertools;
 
 pub mod line_breaks;
-pub mod code_line_breaks;
+pub mod normalize_code_blocks;
 pub mod remove_emojis;
 pub mod normalize;
 pub mod shell_pipe;
@@ -18,19 +18,45 @@ pub mod pandoc;
 pub mod read_toc;
 pub mod file;
 
+const MARKDOWN_OPTIONS: &'static str = "markdown+grid_tables+pipe_tables+raw_html+implicit_figures+footnotes+intraword_underscores+auto_identifiers-inline_code_attributes";
+
 fn main() {
     let toc = file::get_file_content("../src/SUMMARY.md").unwrap();
 
     let mut book = String::new();
 
     book.push_str(include_str!("book_meta.yml"));
-    book.push_str("\n\n");
+    book.push_str("\n");
+
+    let pandoc_options = format!(
+        "--from={markdown_options} --to={markdown_options} --base-header-level={header_level} --indented-code-classes=rust --atx-headers",
+        markdown_options = MARKDOWN_OPTIONS, header_level = 3
+    );
 
     for chapter in &read_toc::get_chapters(&toc) {
+        let path = format!("../src/{}", &chapter.file);
+        let file = file::get_file_content(&path)
+            .ok().expect(&format!("Couldn't read {}", &path));
+
+        print!("{file}: Read ok.", file = &path);
+
+        let mut content = pandoc::run(&pandoc_options, &file)
+            .map_err(|err| format!("pandoc error: {}", err.description()))
+            .unwrap();
+
+        content = normalize::normalize(&content)
+            .map_err(|err| format!("normalize error: {}", err.description()))
+            .unwrap();
+
+        book.push_str("\n");
         book.push_str(&chapter.headline);
+        book.push_str("\n");
+        book.push_str(&content);
+
+        println!(" Processing ok. {} bytes added.", &content.len());
     }
 
     file::write_string_to_file(&book, "dist/_all.md").unwrap();
 
-    println!("Wrote {} lines.", book.lines().count());
+    println!("Wrote {} bytes.", book.len());
 }
--- src/normalize.rs
+++ src/normalize.rs
@@ -1,24 +1,27 @@
 use std::error::Error;
 use regex::Regex;
 
-pub use code_line_breaks;
+pub use normalize_code_blocks::*;
 
 pub fn normalize_links(input: &str) -> Result<String, Box<Error>> {
-    let cross_section_link = Regex::new(r"\]\(([\w\-\_]+)\.html\)").unwrap();
+    let cross_section_link = Regex::new(r"]\(([\w-_]+)\.html\)").unwrap();
 
     let output = input
-    .replace("../std", "http://doc.rust-lang.org/std")
-    .replace("../reference", "http://doc.rust-lang.org/reference")
-    .replace("../rustc", "http://doc.rust-lang.org/rustc")
-    .replace("../syntax", "http://doc.rust-lang.org/syntax")
-    .replace("../core", "http://doc.rust-lang.org/core");
+    .replace(r"../std", r"http://doc.rust-lang.org/std")
+    .replace(r"../reference", r"http://doc.rust-lang.org/reference")
+    .replace(r"../rustc", r"http://doc.rust-lang.org/rustc")
+    .replace(r"../syntax", r"http://doc.rust-lang.org/syntax")
+    .replace(r"../core", r"http://doc.rust-lang.org/core");
 
-    Ok(cross_section_link.replace_all(&output, "](#sec--$1)"))
+    Ok(cross_section_link.replace_all(&output, r"](#sec--$1)"))
 }
 
 
 pub fn normalize(input: &str) -> Result<String, Box<Error>> {
-    let mut output = try!(code_line_breaks::break_code_blocks(&input, 87, "↳ "));
+    let mut output;
+
+    output = try!(break_code_blocks(&input, 87, "↳ "));
     output = try!(normalize_links(&output));
+
     Ok(output)
 }
