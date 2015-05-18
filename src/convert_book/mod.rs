a6ea7bf27969c1a851c7ded41e7ed73d8f7b2de7
Pascal Hertleif <killercup@gmail.com>
2015-05-18 11:20:10 +0200

    Move Render Book Function to `convert_book`

--- src/convert_book/mod.rs
+++ src/convert_book/mod.rs
@@ -4,3 +4,34 @@ pub mod index;
 pub mod markdown;
 pub mod options;
 pub mod pandoc;
+
+use std::error::Error;
+use helpers;
+use convert_book::pandoc::save_as;
+
+/// Render book in different formats
+pub fn render_book() -> Result<(), Box<Error>> {
+    let book = try!(markdown::to_single_file(
+        "../src/SUMMARY.md",
+        &format!(include_str!("../book_meta.yml"), release_date = options::RELEASE_DATE)
+    ));
+
+    try!(helpers::file::write_string_to_file(&book,
+        &format!("dist/trpl-{}.md", options::RELEASE_DATE)
+    ));
+    println!("[✓] {}", "MD");
+
+    try!(save_as(&book, "html", options::HTML));
+    try!(save_as(&book, "epub", options::EPUB));
+    try!(save_as(&book, "tex", options::LATEX));
+
+    let plain_book = helpers::remove_emojis::remove_emojis(&book);
+    try!(save_as(&plain_book, "a4.pdf",
+        &format!(r"{} --variable papersize=a4paper", options::LATEX)
+    ));
+    try!(save_as(&plain_book, "letter.pdf",
+        &format!(r"{} --variable papersize=letterpaper", options::LATEX)
+    ));
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,8 @@ extern crate regex;
 pub mod helpers;
 pub mod convert_book;
 
-use std::error::Error;
-use convert_book::options;
-use convert_book::pandoc::save_as;
-
-fn render_book() -> Result<(), Box<Error>> {
-    let book = try!(convert_book::markdown::to_single_file(
-        "../src/SUMMARY.md",
-        &format!(include_str!("book_meta.yml"), release_date = options::RELEASE_DATE)
-    ));
-
-    try!(helpers::file::write_string_to_file(&book,
-        &format!("dist/trpl-{}.md", options::RELEASE_DATE)
-    ));
-    println!("[✓] {}", "MD");
-
-    try!(save_as(&book, "html", options::HTML));
-    try!(save_as(&book, "epub", options::EPUB));
-    try!(save_as(&book, "tex", options::LATEX));
-
-    let plain_book = helpers::remove_emojis::remove_emojis(&book);
-    try!(save_as(&plain_book, "a4.pdf",
-        &format!(r"{} --variable papersize=a4paper", options::LATEX)
-    ));
-    try!(save_as(&plain_book, "letter.pdf",
-        &format!(r"{} --variable papersize=letterpaper", options::LATEX)
-    ));
-
-    Ok(())
-}
-
 fn main() {
-    render_book().unwrap();
+    convert_book::render_book().unwrap();
 
     let index = convert_book::index::render_index("dist/").unwrap();
     helpers::file::write_string_to_file(&index, "dist/index.html").unwrap();
