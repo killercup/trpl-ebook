a6e47d00eb915f0d0ad1f1f3436ad2ea80ff8ed6
Pascal Hertleif <killercup@gmail.com>
2015-05-21 15:13:06 +0200

    Normalize Subsection Links
    I.e., links to section of other files

--- src/helpers/normalize.rs
+++ src/helpers/normalize.rs
@@ -3,16 +3,20 @@ use std::error::Error;
 use helpers::normalize_code_blocks::*;
 
 fn normalize_links(input: &str) -> Result<String, Box<Error>> {
-    let cross_section_link = regex!(r"]\(([\w-_]+)\.html\)");
-
-    let output = input
+    let mut output = input
     .replace(r"../std", r"http://doc.rust-lang.org/std")
     .replace(r"../reference", r"http://doc.rust-lang.org/reference")
     .replace(r"../rustc", r"http://doc.rust-lang.org/rustc")
     .replace(r"../syntax", r"http://doc.rust-lang.org/syntax")
     .replace(r"../core", r"http://doc.rust-lang.org/core");
 
-    Ok(cross_section_link.replace_all(&output, r"](#sec--$1)"))
+    let cross_section_link = regex!(r"]\((?P<file>[\w-_]+)\.html\)");
+    output = cross_section_link.replace_all(&output, r"](#sec--$file)");
+
+    let cross_subsection_link = regex!(r"]\((?P<file>[\w-_]+)\.html#(?P<subsection>[\w-_]+)\)");
+    output = cross_subsection_link.replace_all(&output, r"](#$subsection)");
+
+    Ok(output)
 }
 
 fn normalize_math(input: &str) -> Result<String, Box<Error>> {
