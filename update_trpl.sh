a75e69c5dcda2ad7a0491fa5612bc62ed2647275
Sunguk Lee <d3m3vilurr@gmail.com>
2016-04-26 18:11:57 +0900

    Helper for TRPL documents

---
+++ update_trpl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+REPO_URL=https://github.com/rust-lang/rust/archive
+
+die() {
+    echo "$@" 1>&2
+    exit 1
+}
+
+BRANCH=$1
+if [ -z $BRANCH ]; then
+    BRANCH=master
+fi
+URL=$REPO_URL/$BRANCH.tar.gz
+rm -rf tmp
+mkdir tmp
+curl -L $URL | tar xz -C tmp || die "Failed to download rust source code"
+rm -rf trpl
+mv ./tmp/rust-*/src/doc/book trpl
+rm -rf tmp
