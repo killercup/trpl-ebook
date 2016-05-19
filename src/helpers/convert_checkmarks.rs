4c948c38f38852b5d53f464debb20f65f5c8f66c
Steven Burns <royalstream@hotmail.com>
2016-05-19 13:46:23 -0600

    correct unicode value for thick checkmark (not used in the document but added for completeness)

--- src/helpers/convert_checkmarks.rs
+++ src/helpers/convert_checkmarks.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 pub fn convert_checkmarks(input: &str) -> String {
-    let checks = Regex::new("[\u{2713}\u{2417}]").unwrap();
+    let checks = Regex::new("[\u{2713}\u{2714}]").unwrap();
     // rely on pandoc's raw latex support
     checks.replace_all(input, "\\checkmark")
 }
